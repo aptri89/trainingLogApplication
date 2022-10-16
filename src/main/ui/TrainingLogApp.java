b5d5d5de29d145a9bca3cb7b32c01d85054096b4
Ava <aptri89@student.ubc.ca>
2022-10-15 20:27:44 -0700

    started ui

--- src/main/ui/TrainingLogApp.java
+++ src/main/ui/TrainingLogApp.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 // application for tracking workouts
 
 public class TrainingLogApp {
+    private Scanner input;
 
 
 
     //EFFECTS: runs the workout tracking application
-    public TrainingLogApp { runTrainingLog();}
+    public TrainingLogApp() {
+        runTrainingLog();
+    }
 
     // MODIFIES: this
     // EFFECTS: processes user input
     private void runTrainingLog() {
+        boolean keepGoing = true;
+        String command = null;
+
+
+
+
 
     }
 
