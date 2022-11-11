691fc4c198b57a9ca0d7f21ea868fc5165d2015e
Ava <aptri89@student.ubc.ca>
2022-11-10 16:54:23 -0800

    created GUI class

---
+++ src/main/ui/TrainingLogViewerUI.java
@@ -0,0 +1,15 @@
+package ui;
+
+// citation for AlarmSystem here **
+
+import javax.swing.*;
+
+
+
+// represents application's main window frame
+public class TrainingLogViewerUI extends JFrame {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+
+}
