8f86f6789802516c87976dc81d2b6c707b59406c
Ava <aptri89@student.ubc.ca>
2022-11-12 14:14:04 -0800

    non-functional buttons now visible

--- src/main/ui/TrainingLogViewerUI.java
+++ src/main/ui/TrainingLogViewerUI.java
@@ -2,6 +2,7 @@ package ui;
 
 // citation for AlarmSystem here **
 
+import model.Swim;
 import model.Workout;
 import persistence.JsonReader;
 import persistence.JsonWriter;
@@ -24,7 +25,6 @@ public class TrainingLogViewerUI extends JFrame {
     private Scanner input;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
-    private KeyBoard kb;
 
 
 
@@ -45,6 +45,12 @@ public class TrainingLogViewerUI extends JFrame {
 
         addButtons();
 
+        controlPanel.setTitle("Training Log Controls");
+
+        controlPanel.pack();
+        controlPanel.setVisible(true);
+        desktop.add(controlPanel);
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
@@ -53,11 +59,14 @@ public class TrainingLogViewerUI extends JFrame {
     // EFFECTS: helper to add buttons for TrainingLogViewer
     private void addButtons() {
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(2,2));
-        buttonPanel.add(new JButton(new AddNewWorkoutAction()));
+        buttonPanel.setLayout(new GridLayout(2,3));
+        buttonPanel.add(new JButton(new AddNewSwimAction()));
+        buttonPanel.add(new JButton(new AddNewBikeAction()));
+        buttonPanel.add(new JButton(new AddNewRunAction()));
         buttonPanel.add(new JButton(new FilterByTypeAction()));
         buttonPanel.add(new JButton(new SaveCurrentAction()));
         buttonPanel.add(new JButton(new LoadPreviousAction()));
+
 
         controlPanel.add(buttonPanel, BorderLayout.SOUTH);
 
@@ -67,51 +76,117 @@ public class TrainingLogViewerUI extends JFrame {
     // MODIFIES: TrainingLog
     // EFFECTS: represents action to be taken when user wants to add a new workout to the
     // current training log
-    private class AddNewWorkoutAction extends AbstractAction {
+    private class AddNewSwimAction extends AbstractAction {
 
-        AddNewWorkoutAction() {
-            super("Add New Workout: ");
+        AddNewSwimAction() {
+            super("Add New Swim");
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            Workout newWorkout = createWorkout();
+
+            // TODO: need to trigger a swim-specific pop-up window for user input and then use code from previous ui
+            //  to record all the inputs
+
+            // TODO: add swim to training log
+
+            // TODO: after all inputs entered and swim is created, need to somehow display it in format:
+            //  type + ": " + title + " (" + date + ")\n"
 
         }
     }
 
-    private Workout createWorkout() {
-        Workout w = new Workout("Swim", "Swim", "October",
-                0,0,0,0);
-        return w; // stub
+    private class AddNewBikeAction extends AbstractAction {
 
+        AddNewBikeAction() {
+            super("Add New Bike");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            // TODO: need to trigger a bike-specific pop-up window for user input and then use code from previous ui
+            //  to record all the inputs
+
+            // TODO: add bike to training log
+
+            // TODO: after all inputs entered and bike is created, need to somehow display it in format:
+            //  type + ": " + title + " (" + date + ")\n"
+
+        }
+    }
+
+    private class AddNewRunAction extends AbstractAction {
+
+        AddNewRunAction() {
+            super("Add New Run");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            // TODO: need to trigger a run-specific pop-up window for user input and then use code from previous ui
+            //  to record all the inputs
+
+            // TODO: add run to training log
+
+            // TODO: after all inputs entered and run is created, need to somehow display it in format:
+            //  type + ": " + title + " (" + date + ")\n"
+
+        }
     }
 
     private class FilterByTypeAction extends AbstractAction {
 
+        FilterByTypeAction() {
+            super("Filter Workouts By Type");
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            // TODO: pop-up with the three types (Swim, Bike and Run) as buttons needs to appear
+
+            // TODO: button input needs to trigger the display in the main window to only show workouts with the type
+            //  pressed by the user
 
         }
     }
 
     private class SaveCurrentAction extends AbstractAction {
 
+        SaveCurrentAction() {
+            super("Save Current Training Log");
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            // TODO: use JsonWriter code (in TrainingLog and Workout) to save the current workouts that have
+            //  been created
+
 
         }
     }
 
     private class LoadPreviousAction extends AbstractAction {
 
+        LoadPreviousAction() {
+            super("Load Previous Training Log");
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
 
+            // TODO: use JsonReader code (idk where it is right now but we'll find it) to load the previously saved
+            //  workouts
+
+            // TODO: add those loaded workouts to this training log
+
+            // TODO: display the loaded workouts
+
         }
     }
-
-
 
 
 
